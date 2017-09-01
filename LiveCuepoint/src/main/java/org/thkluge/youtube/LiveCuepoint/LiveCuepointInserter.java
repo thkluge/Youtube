531d1079a6ea84417d1147f9368e91ac6b2020a6
thkluge <thkluge@localhost>
2017-09-01 09:58:54 +0200

    Initial commit.

---
+++ LiveCuepoint/src/main/java/org/thkluge/youtube/LiveCuepoint/LiveCuepointInserter.java
@@ -0,0 +1,13 @@
+package org.thkluge.youtube.LiveCuepoint;
+
+/**
+ * Hello world!
+ *
+ */
+public class LiveCuepointInserter 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
